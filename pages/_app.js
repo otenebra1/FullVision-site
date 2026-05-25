896e150586dc430ff7bca0e4e807ba4c0eb1ebb4
Jose Luiz de Oliveira <joseluiz1912@gmail.com>
2026-05-25 16:31:21 -0300

    fix: corrige _app.js duplicado

--- pages/_app.js
+++ pages/_app.js
@@ -2,31 +2,17 @@ import '@/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import WhatsAppButton from '@/components/WhatsAppButton';
 import { Analytics } from '@vercel/analytics/react';
-
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-      <WhatsAppButton />
-      <Analytics /> {/* Adicione esta linha */}
-    </>
-  );
-}
-
-// 1. Importações da animação
 import { useEffect } from 'react';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // Importa o CSS das animações
+import 'aos/dist/aos.css';
 
 export default function App({ Component, pageProps }) {
-  
-  // 2. Este código liga as animações assim que o site carrega
+
   useEffect(() => {
     AOS.init({
-      duration: 800,     // A animação demora 0.8 segundos (suave)
-      once: true,        // A animação só acontece uma vez (não repete ao subir a página)
-      easing: 'ease-out' // Efeito de abrandamento no final
+      duration: 800,
+      once: true,
+      easing: 'ease-out'
     });
   }, []);
 
@@ -35,6 +21,7 @@ export default function App({ Component, pageProps }) {
       <Navbar />
       <Component {...pageProps} />
       <WhatsAppButton />
+      <Analytics />
     </>
   );
 }
