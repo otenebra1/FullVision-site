eb01891ab00262fefa826e3e219ae9544f9fd3ab
Jose Luiz de Oliveira <joseluiz1912@gmail.com>
2026-05-25 09:34:43 -0300

    design: adicionando animacoes AOS na pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Redirect from "@/components/Redirect";
 import Slider from "@/components/Slider";
 import HomeImage from "@/components/Hero";
@@ -6,22 +7,38 @@ import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <>
-      {/* 1. O Slider dinâmico no topo, um pouco mais estreito */}
-      <Slider slides={SliderData} />
+    // Adicionado o fundo preto padrão e overflow-hidden para não ter barras de scroll indesejadas na animação
+    <div className="bg-[#000000] min-h-screen flex flex-col overflow-hidden">
+      <Head>
+        <title>Início | FullVision Tracking</title>
+        <meta name="description" content="Gestão de operação logística e segurança da frota." />
+      </Head>
 
-      {/* 2. A div corporativa com a imagem de fundo original, texto e botão comercial */}
-      <HomeImage 
-        heading='Fullvision Tracking' 
-        message='Gestão de operação logística e segurança da frota.' 
-      />
+      <main className="flex-grow">
+        {/* 1. O Slider dinâmico no topo, surgindo suavemente */}
+        <div data-aos="fade-down" data-aos-duration="1000">
+          <Slider slides={SliderData} />
+        </div>
 
-      {/* 3. Seção do Card de acesso à plataforma de rastreamento */}
-      <div className="w-full flex justify-center items-center my-12 px-4">
-        <Redirect />
-      </div>
+        {/* 2. A div corporativa com a imagem de fundo original */}
+        <div data-aos="fade-up" data-aos-delay="150">
+          <HomeImage 
+            heading='Fullvision Tracking' 
+            message='Gestão de operação logística e segurança da frota.' 
+          />
+        </div>
+
+        {/* 3. Seção do Card de acesso à plataforma de rastreamento */}
+        <div 
+          data-aos="zoom-in" 
+          data-aos-delay="300" 
+          className="w-full flex justify-center items-center my-16 px-4"
+        >
+          <Redirect />
+        </div>
+      </main>
 
       <Footer />
-    </>
+    </div>
   )
 }
