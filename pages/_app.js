ba07be6b81db3170cba67645173aafbaa1e865e3
Jose Luiz de Oliveira <joseluiz1912@gmail.com>
2026-05-25 10:02:29 -0300

    design: removendo rotacao do logo na navbar e adicionando opacidade sutil

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import '@/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import WhatsAppButton from '@/components/WhatsAppButton';
+import { Analytics } from '@vercel/analytics/react';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <WhatsAppButton />
+      <Analytics /> {/* Adicione esta linha */}
+    </>
+  );
+}
 
 // 1. Importações da animação
 import { useEffect } from 'react';
