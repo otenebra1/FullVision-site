c4dcf4e9a2ff09d592dc15f28410153b7f131f9c
zeca <joseluiz1912@gmail.com>
2026-07-23 11:29:57 -0300

    atualizar tipografia para Plus Jakarta Sans

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,15 @@ import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+// 1. Importação da nova fonte premium
+import { Plus_Jakarta_Sans } from 'next/font/google';
+
+// 2. Configuração dos pesos da fonte que vamos usar
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
 export default function App({ Component, pageProps }) {
 
   useEffect(() => {
@@ -17,11 +26,12 @@ export default function App({ Component, pageProps }) {
   }, []);
 
   return (
-    <>
+    // 3. Envolver todo o projeto (Navbar, Componentes e Botões) com a classe da fonte
+    <div className={fontSans.className}>
       <Navbar />
       <Component {...pageProps} />
       <WhatsAppButton />
       <Analytics />
-    </>
+    </div>
   );
 }
